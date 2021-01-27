3a7076f85d2df6311c9d78c7a0688259ab151ff6
Jorge da Costa Pereira Junior <juniinho.pereira1@gmail.com>
2021-01-26 22:17:48 -0300

    upgrade

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,78 @@ export const QuizContainer = styled.div`
   }
 `;
 
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4caf50;
+  background-color: #1c1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.header`
+  padding: 24px 32px 32px 32px; 
+  & > *::first-child {
+    margin-top: 0;
+  }
+  & > *::last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style:none;
+    padding: 0;
+  }
+`;
+
 export default function Home() {
   return (
     <BackgroundImage>
-      shuashua
+      <QuizContainer>
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+              <h1>The Legend of Zelda</h1>
+
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Header>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <Widget.Header>
+            <h1>The Legend of Zelda</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Header>
+          </Widget.Content>
+        </Widget>
+
+      </QuizContainer>
     </BackgroundImage>
   );
 }
