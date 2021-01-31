209f8ab9d490e0dea65bbbdbd14c0db63a7dcb84
Jorge da Costa Pereira Junior <juniinho.pereira1@gmail.com>
2021-01-31 11:25:47 -0300

    atualização

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import styled from 'styled-components'
 import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
  //const Title = styled.h1`
   //font-size: 50px;
   //color: ${({ theme }) => theme.colors.primary};
 //`
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
+//const BackgroundImage = styled.div`
+  //background-image: url(${db.bg});
+  //flex: 1;
+  //background-size: cover;
+  //background-position: center;
+//`;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -24,78 +29,30 @@ export const QuizContainer = styled.div`
   }
 `;
 
-const Widget = styled.div`
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid #4caf50;
-  background-color: #1c1814;
-  border-radius: 4px;
-  overflow: hidden;
-
-  h1, h2, h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-  p {
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1;
-  }
-`;
-
-Widget.Header = styled.header`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 18px 32px;
-  background-color: ${({ theme }) => theme.colors.primary};
-
-  * {
-    margin: 0;
-  }
-`;
-
-Widget.Content = styled.header`
-  padding: 24px 32px 32px 32px; 
-  & > *::first-child {
-    margin-top: 0;
-  }
-  & > *::last-child {
-    margin-bottom: 0;
-  }
-  ul {
-    list-style:none;
-    padding: 0;
-  }
-`;
-
 export default function Home() {
   return (
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
           <Widget.Content>
-            <Widget.Header>
-              <h1>The Legend of Zelda</h1>
-
-              <p>lorem ipsum dolor sit amet...</p>
-            </Widget.Header>
+            <p>{db.description}</p>
           </Widget.Content>
         </Widget>
 
         <Widget>
           <Widget.Content>
-            <Widget.Header>
-            <h1>The Legend of Zelda</h1>
+            <h1>Quizes da Galera</h1>
 
             <p>lorem ipsum dolor sit amet...</p>
-            </Widget.Header>
           </Widget.Content>
         </Widget>
-
+        <Footer />
       </QuizContainer>
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/JuniinhoPereira" />
+    </QuizBackground>
   );
 }
